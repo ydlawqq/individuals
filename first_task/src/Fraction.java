f3aca139e6394683871f18484346788a58b0bfa6
Daniil <volkodot@mail.ru>
2025-11-11 14:05:26 +0300

    Первое задание выполнено

---
+++ first_task/src/Fraction.java
@@ -0,0 +1,19 @@
+public class Fraction {
+    int ch, zn;
+
+    Fraction(int ch, int zn){
+        this.ch = ch;
+        this.zn = zn;
+    }
+    public int getCh(){
+        return this.ch;
+    }
+    public int getZn(){
+        return this.zn;
+    }
+    Fraction
+
+
+
+
+}
